1346d9435f9075988e1af730c3c42fc51223f754
Mete Rodoper <mrodoper@gmail.com>
2017-09-13 01:29:35 -0700

    setup: add inputrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-cp ./.vimrc ~/.vimrc
+cp ./.vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+cp ./.inputrc ~/
+
+cp -r ./.vim ~/
