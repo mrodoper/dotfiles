39987ca5edba241600c88a0067cfd9f35e007162
Mete Rodoper <mrodoper@gmail.com>
2017-10-07 15:22:03 -0700

    git: add git config

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Backing up VIM"
 # backup vim
 cp ~/.vimrc ./
 mkdir -p ./.vim
@@ -7,9 +8,13 @@ cp -r ~/.vim/plugin ./.vim
 cp -r ~/.vim/ftplugin ./.vim
 cp ~/.ycm_extra_conf.py ./
 
+echo "Backing up TMUX"
 # backup tmux
 cp ~/.tmux.conf ./
 cp -r ~/.tmux ./
 
+echo "Backing up others (inputrc, git, ...)"
 # backup others
 cp ~/.inputrc ./
+cp ~/.gitconfig ./
+cp ~/.gitignore_global ./
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Installing the needed ubuntu packages"
 # install needed packages
 sudo apt-get install vim
 sudo apt-get install cscope
@@ -8,6 +9,7 @@ sudo apt-get install python-dev python3-dev
 sudo apt-get install tmux
 sudo apt-get install --assume-yes xclip
 
+echo "Setting up VIM"
 # setup vim
 cp ./.vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -19,10 +21,16 @@ cp ./.ycm_extra_conf.py ~/
 
 cp -r ./.vim ~/
 
+echo "Setting up TMUX"
 # setup tmux
 cp ./.tmux.conf ~/
 cp -r ./.tmux ~/
 sudo ln -s ~/.tmux/tvim /usr/bin/tvim
 
+echo "Setting up others (inputrc, git, ...)"
 # setup others
 cp ./.inputrc ~/
+cp ./.gitconfig ~/
+cp ./.gitignore_global ~/
+
+echo "Make sure the git config has the correct user.name and email"
