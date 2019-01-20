bd52ca1f55eaf850d43b7bb3e93128c8bd918d29
Mete Rodoper <mrodoper@gmail.com>
2019-01-20 00:01:38 -0800

    {setup,fzf}: choose all default settings

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ else
   git pull
 	popd
 fi
-~/.fzf/install
+~/.fzf/install --all
 
 echo "Setting up oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
